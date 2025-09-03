e1dcf3e5ed8793d1a30a7925688895bd4554da36
sebranchett <s.e.branchett@tudelft.nl>
2025-09-03 14:17:30 +0200

    before making Yambo input a function

--- qey.py
+++ qey.py
@@ -1,38 +1,67 @@
 import numpy as np
 import yambopy as ymb
+import sys
 
+logfilename = 'log.txt'
 yambo_dir = "./MoTe2/SAVE"
 yambo_file = "ns.db1"
-yambo = ymb.YamboElectronsDB.from_db_file(yambo_dir, yambo_file)
-lattice = ymb.YamboLatticeDB.from_db_file(yambo_dir + "/" + yambo_file)
-yambo_eigs_ibz = yambo.eigenvalues_ibz
+TrueBG = 0.9
 
-# print out the dimensions of yambo_eigs_ibz
-print("Shape of IBZ eigenvalues:", yambo_eigs_ibz.shape)
 
-yambo.expandEigenvalues()
-yambo_eigs = yambo.eigenvalues
-print("Shape of eigenvalues:", yambo_eigs.shape)
-print("Number of occupied bands:", yambo.nbandsv)
-print("Value at (0, 0, 91):", yambo_eigs[0][0][91])
-print("Value at (0, 0, 92):", yambo_eigs[0][0][92])
+def log(info=''):
+    print(info)
+    with open(logfilename, 'a') as logfile:
+        logfile.write(info)
+        logfile.write('\n')
 
-print("Summary information about yambo_eigs:")
-print("Shape:", yambo_eigs.shape)
-print("Max:", np.max(yambo_eigs))
-print("Min:", np.min(yambo_eigs))
-print("Energy gaps:", yambo.energy_gaps())
 
-max_valence = np.max(yambo_eigs[:, :, :92])
-min_conduction = np.min(yambo_eigs[:, :, 92:])
-print("Max valence band energy:", max_valence)
-print("Min conduction band energy:", min_conduction)
-print("Calculated band gap:", min_conduction - max_valence)
+def msg_title(msg):
+    n = (75 - 2 - len(msg))//2
+    log("#"*n + ' ' + msg + ' ' + "#"*n)
 
-print("Reciprocal lattice:")
-print(lattice.rlat)
 
+def msg_error(msg):
+    log()
+    log("#"*75)
+    log('# Error:')
+    log('# ' + msg)
+    log("#"*75)
+    log()
+
+
+log()
+msg_title('INITIALIZING INPUT DATA')
+log()
+log("Loading Yambo file from: \n" + yambo_dir + "/" + yambo_file)
+log()
+try:
+    yambo = ymb.YamboElectronsDB.from_db_file(yambo_dir, yambo_file)
+    lattice = ymb.YamboLatticeDB.from_db_file(yambo_dir + "/" + yambo_file)
+except FileNotFoundError:
+    msg_error(
+        'Yambo ns.db1 file cannot be loaded, '
+        'check given path for Yambo file'
+    )
+    sys.exit()
+
+log("File loaded")
 nkpt = lattice.nkpoints
+info = [nkpt, yambo.nbands]
+log("k-points: {} \nBands: {}".format(*info))
+log()
+
+#   ############################################
+
+#   ############### gather kpoints #############
+
+log(
+    "Gathering k-points in Cartesian coordinates & "
+    "calculating vector magnitudes.."
+    )
+
+reciprocal_lattice = lattice.rlat
+reciprocal_lattice_inv = np.linalg.inv(reciprocal_lattice)
+
 kpoints = np.zeros([nkpt, 4])
 kpoints[:, :3] = lattice.car_kpoints
 # The magnitude of the k-vectors are calculated in order to speed up the
@@ -40,5 +69,33 @@ kpoints[:, :3] = lattice.car_kpoints
 for k in range(kpoints.shape[0]):
     kpoints[k, 3] = np.sqrt(np.dot(kpoints[k, :3], kpoints[k, :3]))
 
-print("K points:")
-print(kpoints)
+log("Done\n")
+
+#   ############################################
+
+#   ############# gather eigenvalues ###########
+log("Gathering energies..")
+
+# Expand the eigenvalues from the IBZ to the full BZ
+yambo.expandEigenvalues()
+
+cbm = yambo.nbandsv
+max_valence_energy = np.max(yambo.eigenvalues[0, :, :cbm])
+min_conduction_energy = np.min(yambo.eigenvalues[0, :, cbm:])
+energies = yambo.eigenvalues[0, :, :] - max_valence_energy
+# sets VBM as 0
+# Since the BG is underestimated in DFT, a scissor operator is used to
+# shift the CB so that it matches with experimental data
+DFT_BG = min_conduction_energy - max_valence_energy
+scissor = TrueBG - DFT_BG
+log('Scissor operation:')
+log(f'Calculated bandgap: {DFT_BG} eV')
+log(f'Experimental bandgap: {TrueBG} eV')
+log(f'Shifting CB with: {scissor:1.6f} eV')
+log('Please check if the calculated bandgap is indeed in eV.')
+log()
+
+energies[:, cbm:] = energies[:, cbm:] + scissor
+
+# return reciprocal_lattice, reciprocal_lattice_inv, \
+#         kpoints, energies
